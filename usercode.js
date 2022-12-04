9f833e8090dd3fd64691745a22f535ac9c606372
SergLapik <118845557+SergLapik@users.noreply.github.com>
2022-12-04 16:10:02 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,7 +1,32 @@
+import requests
+import random
+from bs4 import BeautifulSoup
 
 
-module.exports = (data) => {
-    data.hello = "HELLO";
+def get_html(url):
+    r = requests.get(url)
+    return r.text
 
-    return data;
-};
+
+def get_jokes(html):
+    result_tab = []
+    soup = BeautifulSoup(html,'lxml')
+    page = soup.find('ul',class_='item-list')
+    jokes = page.find_all('li')
+    
+    for joke in jokes:
+        result_tab.append(joke.p.text.strip())
+    return result_tab
+
+
+def main():
+    url1 = 'http://anekdoty.ru/pro-robotov/'
+    url2 = 'http://anekdoty.ru/'
+    html = get_html(url1)
+    jokes = get_jokes(html)
+    jokes.extend(get_jokes(get_html(url2)))
+    print(random.choice(jokes))
+    input()
+
+if name == 'main':
+    main()
