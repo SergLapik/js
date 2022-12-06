007d82ddf260bf75e589cca23474f70a15c0554d
SergLapik <118845557+SergLapik@users.noreply.github.com>
2022-12-06 19:05:10 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -2,11 +2,8 @@ module.exports = (data) => {
 var header = '1';  
 var payload = '2';
 var SECRET_KEY = '3';
-    
-var unsignedToken = base64urlEncode(header) + '.' + base64urlEncode(payload)
-var signature = HMAC-SHA256(unsignedToken, SECRET_KEY)
 
-data.token = encodeBase64Url(header) + '.' + encodeBase64Url(payload) + '.' + encodeBase64Url(signature)
+data.token = header + payload + SECRET_KEY;
     
     return data;
 };
