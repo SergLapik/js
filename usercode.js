98b4e7b2ba5c4d97a73c010753884cb4b565748c
SergLapik <118845557+SergLapik@users.noreply.github.com>
2022-12-06 19:13:37 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,9 +1,11 @@
 module.exports = (data) => {
-var header = '1';  
-var payload = '2';
+    
+var header = { "alg": "HS256", "typ": "JWT"};
+var payload = {"userId": "adc720c4-1c51-4c61-8197-d3980048fe15"};
+
 var SECRET_KEY = '3';
 
-data.token = header + payload + SECRET_KEY;
+data.token = SECRET_KEY;
     
     return data;
 };
