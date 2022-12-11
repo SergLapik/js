a048d39ef804579cdcbf25d69623e241acfcf890
SergLapik <118845557+SergLapik@users.noreply.github.com>
2022-12-11 17:02:53 +0200

    Update usercode.py

--- usercode.py
+++ usercode.py
@@ -1,19 +1,17 @@
 import jwt
 def handle(data):
-	
-     payload_data = {
-      "iat": data["now"],
-      "exp": data["future"],
-      "jti": data["jti"],
-      "application_id": data["application_id"]
+	payload_data = {
+      		"iat": data["now"],
+      		"exp": data["future"],
+      		"jti": data["jti"],
+      		"application_id": data["application_id"]
 	 }
-
-    key = data["key"]
-    token = jwt.encode(
-      payload=payload_data,
-      key=key,
-      algorithm='HS256'
+	key = data["key"]
+    	token = jwt.encode(
+      	payload=payload_data,
+      	key=key,
+      	algorithm='HS256'
     )
    
-    data["token"] = token
-    return data
+    	data["token"] = token
+    	return data
